7bb90901717ebf8cf0883664bb056ba21dfa4163
Sn0w3y <hallerjo97@gmail.com>
2024-04-28 00:36:20 +0200

    Update goodwe.py

--- goodwe.py
+++ goodwe.py
@@ -10,11 +10,10 @@ FORWARD_PORT = 20001
 LISTEN_IP = '0.0.0.0'
 LISTEN_PORT = 20001
 HEADER_LENGTH = 52
-AES_KEY = b'\xFF' * 16  # Security note: This should be generated and stored securely
+AES_KEY = b'\xFF' * 16
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 
 def hex_to_celsius(hex_data):
     """Convert hexadecimal temperature from deci-Celsius to Celsius."""
@@ -22,17 +21,14 @@ def hex_to_celsius(hex_data):
     temp_deci_celsius = int(hex_value, 16)
     return temp_deci_celsius / 10
 
-
 def hex_to_soc(hex_data):
     """Convert hexadecimal state of charge to integer percentage."""
     hex_value = hex_data[1238:1242]
     return int(hex_value, 16)
 
-
 def hex_to_volt(hex_data):
     """Placeholder for voltage conversion."""
     return "ToDo"
-
 
 def decrypt_data(key, iv, data):
     """Decrypt data using AES-CBC."""
@@ -40,14 +36,12 @@ def decrypt_data(key, iv, data):
     decryptor = cipher.decryptor()
     return decryptor.update(data) + decryptor.finalize()
 
-
 def forward_data(data):
     """Forward data to predefined IP and port, and return the response."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((FORWARD_IP, FORWARD_PORT))
         sock.sendall(data)
         return sock.recv(1024)
-
 
 def handle_connection(connection):
     """Handle incoming connections and process data."""
@@ -73,6 +67,8 @@ def handle_connection(connection):
 
             decrypted_data = decrypt_data(AES_KEY, iv, data)
 
+            year, month, day, hour, minute, second = [int(x) for x in timestamp]
+            logging.info(f"Date-Time: {day:02}-{month:02}-{2000 + year:04} {hour:02}:{minute:02}:{second:02}")
             logging.info(f"Temperature: {hex_to_celsius(decrypted_data.hex())}°C")
             logging.info(f"State of Charge: {hex_to_soc(decrypted_data.hex())}%")
             logging.info(f"Voltage: {hex_to_volt(decrypted_data.hex())}")
@@ -81,7 +77,6 @@ def handle_connection(connection):
         logging.error(f"An error occurred: {e}")
     finally:
         connection.close()
-
 
 def listen_on_port():
     """Listen on a specified port and handle connections."""
@@ -96,7 +91,6 @@ def listen_on_port():
                 handle_connection(connection)
         except KeyboardInterrupt:
             logging.info("Server shutting down.")
-
 
 if __name__ == "__main__":
     listen_on_port()
